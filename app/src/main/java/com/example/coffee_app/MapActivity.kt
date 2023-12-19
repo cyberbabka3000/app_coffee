f896339b2f18d2b6ea159d1ea49ada8fdc2f2a5a
SUS <olgagorhkovs@gmail.com>
2023-12-19 12:08:04 +0300

    Рабочие карты, без определения местоположения, но с меточкой :3

--- app/src/main/java/com/example/coffee_app/MapActivity.kt
+++ app/src/main/java/com/example/coffee_app/MapActivity.kt
@@ -2,8 +2,13 @@ package com.example.coffee_app
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.map.MapObjectTapListener
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
 
 class MapActivity : Activity() {
 
@@ -11,6 +16,15 @@ class MapActivity : Activity() {
     private val apiKey = "fb7a6632-3557-45b2-a596-384b5f203c2b"
 
     private var mapView: MapView? = null
+
+    private val placemarkTapListener = MapObjectTapListener { _, point ->
+        Toast.makeText(
+            this@MapActivity,
+            "Tapped the point (${point.longitude}, ${point.latitude})",
+            Toast.LENGTH_SHORT
+        ).show()
+        true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +34,27 @@ class MapActivity : Activity() {
 
         setContentView(R.layout.map)
         mapView = findViewById<MapView>(R.id.mapview)
+
+        mapView?.map?.move(
+            CameraPosition(
+                Point(59.935493, 30.327392),
+                /* zoom = */ 17.0f,
+                /* azimuth = */ 150.0f,
+                /* tilt = */ 30.0f
+            )
+        )
+
+        val mapObjects = mapView?.map?.mapObjects
+        val imageProvider = ImageProvider.fromResource(this, R.drawable.placemark_icon)
+        val placemark = mapObjects?.addPlacemark(Point(59.935493, 30.327392))
+        placemark?.setIcon(imageProvider)
+
+        placemark?.addTapListener(placemarkTapListener)
     }
 
     override fun onStop() {
         // Activity onStop call must be passed to both MapView and MapKit instance.
-        mapView!!.onStop()
+        mapView?.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
@@ -33,6 +63,6 @@ class MapActivity : Activity() {
         // Activity onStart call must be passed to both MapView and MapKit instance.
         super.onStart()
         MapKitFactory.getInstance().onStart()
-        mapView!!.onStart()
+        mapView?.onStart()
     }
 }
